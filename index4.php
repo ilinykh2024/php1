31b00090f4ca6e110597f912b9670df173769679
AndreyI <vektor-v@list.ru>
2024-10-23 21:52:26 +0300

    index4_23_10

--- index4.php
+++ index4.php
@@ -1,6 +1,10 @@
 <?php
+echo "Введите свое имя";
+$name = trim(fgets(STDIN));
+echo "Введите отчество";
+$secondname = trim(fgets(STDIN));
+echo "Введите своюфамилию";
+$lastname = trim(fgets(STDIN));
+//$fullname = $name . " " . $secondname . " " . $lastname;
 
-
-
-
-phpinfo()
+echo $name .  $secondname . $lastname;//$fullname;
