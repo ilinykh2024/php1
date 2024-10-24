e914fbf01b2a265a81887d3d492d513938f85acf
AndreyI <vektor-v@list.ru>
2024-10-24 21:31:32 +0300

    index3.php_24_10

--- index3.php
+++ index3.php
@@ -3,7 +3,9 @@ $number1 = trim(fgets(STDIN));
 $number2 = trim(fgets(STDIN));
 if (!is_numeric($number1) or !is_numeric($number2)) {
 fwrite(STDERR,"Пожайлуста, введите число");
+exit(1);
 }
+
 if ($number2 == 0){
     fwrite(STDERR,"Делить на 0 нельзя");
 }
