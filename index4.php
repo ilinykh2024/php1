7f72e914febf4a634fc37572a77fe55de94e2e67
AndreyI <vektor-v@list.ru>
2024-10-25 19:06:23 +0300

    index4_25_10

--- index4.php
+++ index4.php
@@ -1,10 +1,13 @@
 <?php
-echo "Введите свое имя";
-$name = trim(fgets(STDIN));
-echo "Введите отчество";
-$secondname = trim(fgets(STDIN));
-echo "Введите своюфамилию";
-$lastname = trim(fgets(STDIN));
-//$fullname = $name . " " . $secondname . " " . $lastname;
-
-echo $name .  $secondname . $lastname;//$fullname;
+echo "Введите свое имя - \n";
+$name        = ucfirst(trim(fgets(STDIN)));
+echo "Введите отчество - \n";
+$secondname  = ucfirst(trim(fgets(STDIN)));
+echo "Введите свою фамилию - \n";
+$lastname    = ucfirst(trim(fgets(STDIN)));
+$fullName    = $name . ' ' . $secondname . ' ' . $lastname;
+$fio         = substr($name, 0, 1) . substr($secondname, 0, 1). substr($lastname, 0, 1);
+$surnameAndInitials = $name . ' ' . substr($secondname, 0, 1). '.'. substr($lastname, 0, 1) . '.';
+echo "Полное имя: " . $fullName , PHP_EOL;
+echo "Фамилия и инициалы: " . $surnameAndInitials , PHP_EOL;
+echo "Аббревиатура: " . $fio  , PHP_EOL;
