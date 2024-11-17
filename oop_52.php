c268616ce4200497e3bba09da636a466bc01d998
AndreyI <vektor-v@list.ru>
2024-11-18 00:08:52 +0300

    oop_52_php

--- oop_52.php
+++ oop_52.php
@@ -8,22 +8,27 @@ $date_begin = date('Y-m-d', mktime(0, 0, 0, $a, 01, $d));
 $days       = cal_days_in_month(CAL_GREGORIAN, date($a), date($d));
 
 $datetime = DateTime::createFromFormat('Y-m-d', $date_begin);
-$workday  = 2;
 
 echo date('F', mktime(0, 0, 0, $a+1, 0, 0)). PHP_EOL;
 echo "date\ti\tday\twork" . PHP_EOL;
 
+dayWorks($datetime, $days);
 
-for ($i = 1; $i <= $days; ++$i)
+function dayWorks ($datetime, $days) {
+    $datetime1 = $datetime;
+    $timing = $days;
+    $workday  = 2;
+    for ($i = 1; $i <= $timing; ++$i)
 {
     ++$workday;
-    if (date('N', strtotime($datetime->format('d-m-Y'))) < 6 and $workday > 2)
+    if (date('N', strtotime($datetime1->format('d-m-Y'))) < 6 and $workday > 2)
     {
         $workday = 0;
     }
-    echo $datetime->format('d-m-Y') . "\t" . $i . "\t" . date('N', strtotime($datetime->format('d-m-Y')));
+    echo $datetime1->format('d-m-Y') . "\t" . $i . "\t" . date('N', strtotime($datetime1->format('d-m-Y')));
     if ($workday == 0)
         echo '+';
     echo PHP_EOL;
-    $datetime->modify('+1 days');
+    $datetime1->modify('+1 days');
+}
 }
